c4b945d77ed59010c68ab3666a68b9a2e061f6d5
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2024-06-29 12:51:26 +0200

    Cleanup code

--- src/main/java/at/aau/streptrack/edge/EdgeApplication.java
+++ src/main/java/at/aau/streptrack/edge/EdgeApplication.java
@@ -9,15 +9,4 @@ public class EdgeApplication {
   public static void main(String[] args) {
     SpringApplication.run(EdgeApplication.class, args);
   }
-
-  //  @Bean
-  //  MqttPahoClientFactory clientFactory(@Value("${hivemq.uri}") String host) {
-  //    var factory = new DefaultMqttPahoClientFactory();
-  //    var options = new MqttConnectOptions();
-  //
-  //    options.setServerURIs(new String[] {host});
-  //    factory.setConnectionOptions(options);
-  //
-  //    return factory;
-  //  }
 }
